fa0f3c135624cbc24771bb3941c1af65cc2ff023
nzewunwagrace94 <nzewunwagraceonyinyechi@gmail.com>
2024-04-24 19:01:51 +0100

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-# installing flask using puppet from pip3
-
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+#install a flask from pip3
+exec { 'install flask':
+  command =>  '/usr/bin/pip3 install Flask==2.1.0',
+  unless  =>  '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
 }
