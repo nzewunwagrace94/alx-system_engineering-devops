8070a79442a537be78940c4d9c16b89783c618f8
nzewunwagrace94 <nzewunwagraceonyinyechi@gmail.com>
2024-04-24 19:18:27 +0100

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-nstall a flask from pip3
-exec { 'install flask':
-  command =>  '/usr/bin/pip3 install Flask==2.1.0',
-  unless  =>  '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
+#install flask from pip3
+
+package { 'puppet-lint':
+  ensure   => '2.1.0',
+  provider => 'gem',
 }
