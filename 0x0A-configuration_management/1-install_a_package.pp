8996ef98247257f68908b3ae75d665d6fbb4483d
nzewunwagrace94 <nzewunwagraceonyinyechi@gmail.com>
2024-06-19 04:10:05 +0100

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,5 @@
-# Using Puppet, install flask from pip3.
-
+# using pippet, insatll flask from pip3
 package { 'flask':
-    ensure   => '2.1.0',
-    provider => 'pip3',
-}
-
-package { 'werkzeug':
-  ensure   => '2.1.1',
-  provider => 'pip3',
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
